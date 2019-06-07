5fd3a301f2d9ce4f1e186f4e10a2c9d26c2cd63f
yuvaraniyuva <40492725+yuvaraniyuva@users.noreply.github.com>
2019-06-06 21:35:15 -0700

    Create set2_6.c

---
+++ set2_6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int n,a[100],i,j,c[100],ca=0;
+  scanf("%d",&n);
+	c[0]=0;
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&a[i]);
+	c[i]=1;
+	}
+	for(i=1;i<n;i++)
+	{
+	 if(a[i]>a[i-1])
+	 c[i]=c[i-1]+1;
+	}
+	for(i=0;i<n;i++)
+	ca+=c[i];
+	printf("%d",ca);
+	return 0;
+}
